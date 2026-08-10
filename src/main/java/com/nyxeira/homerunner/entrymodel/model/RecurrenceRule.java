805529048b3e596fd700bba52ead43e28a42d3ee
Hugo <coding.raclette@proton.me>
2026-08-10 23:25:53 +0200

    "interval" est un mot réservé SQL (H2)

--- src/main/java/com/nyxeira/homerunner/entrymodel/model/RecurrenceRule.java
+++ src/main/java/com/nyxeira/homerunner/entrymodel/model/RecurrenceRule.java
@@ -14,6 +14,7 @@ public class RecurrenceRule {
     @Enumerated(EnumType.STRING)
     Frequency frequency; // Basée sur un enum non développé explicitement lors de la conception
 
+    @Column(name = "recurrence_interval") // "interval" est un mot réservé SQL (H2)
     int interval;
 
     LocalDate until;
