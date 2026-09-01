0100e56d7a257f43050719e02fa97ef943e89132
Hugo <coding.raclette@proton.me>
2026-09-01 20:14:09 +0200

    Ajout d'une méthode pour récupérer les utilisateurs qui n'ont pas un rôle

--- src/main/java/com/nyxeira/homerunner/usermanagement/repositories/UserRepository.java
+++ src/main/java/com/nyxeira/homerunner/usermanagement/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.nyxeira.homerunner.usermanagement.model.User;
 import com.nyxeira.homerunner.usermanagement.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByRole(UserRole role);
     Optional<User> findByLogin(String login);
+    List<User> findAllByRoleNot(UserRole role);
 }
