c50259612ee64101076ed6510920b54f68eddf3b
Hugo <coding.raclette@proton.me>
2026-09-02 23:45:46 +0200

    Ajout de getters/setters

--- src/main/java/com/nyxeira/homerunner/entrymodel/model/Entry.java
+++ src/main/java/com/nyxeira/homerunner/entrymodel/model/Entry.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 import static jakarta.persistence.InheritanceType.SINGLE_TABLE;
