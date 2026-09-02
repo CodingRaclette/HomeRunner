8774078f95e9cc893f0384b8e385b621f0111fb0
Hugo <coding.raclette@proton.me>
2026-09-02 23:57:46 +0200

    Nettoyage de assignee, désormais on ne parle que de participants, ça n'a pas de sens de séparer les deux

--- src/main/java/com/nyxeira/homerunner/entrymodel/model/Trackable.java
+++ src/main/java/com/nyxeira/homerunner/entrymodel/model/Trackable.java
@@ -7,8 +7,6 @@ import java.util.Set;
 public interface Trackable {
 
     boolean isDone();
-    Set<User> getAssignees();
-    void setAssignees(Set<User> assignees);
     Set<User> getContributors();
     void addContributor(User contributor);
     void removeContributor(User contributor);
